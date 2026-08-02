00e8e132ce054cdea462c4fa66a5b2a219bea430
Matthew Bland <mabla0531@gmail.com>
2026-08-01 19:51:32 -0500

    forgor pull oopage

--- init.lua
+++ init.lua
@@ -4,4 +4,9 @@ require("config.options")
 require("config.keymaps")
 
 -- set colorscheme
-vim.cmd.colorscheme("monokai-pro-ristretto")
+vim.cmd.colorscheme("gruber-darker")
+
+vim.api.nvim_set_hl(0, "DiagnosticUnderlineHint", { underline = true, sp = "#e4e4ef" })
+vim.api.nvim_set_hl(0, "DiagnosticUnderlineInfo", { underline = true, sp = "#e4e4ef" })
+vim.api.nvim_set_hl(0, "DiagnosticUnderlineWarn", { underline = true, sp = "#ffdd33" })
+vim.api.nvim_set_hl(0, "DiagnosticUnderlineError", { underline = true, sp = "#f43841" })
---
+++ lua/plugins/gruber-darker.lua
@@ -0,0 +1,15 @@
+return {
+	{
+		"blazkowolf/gruber-darker.nvim",
+		opts = {
+			bold = false,
+			italic = {
+				strings = false,
+				comments = false,
+				operators = false,
+				folds = false,
+			},
+			undercurl = false,
+		},
+	},
+}
