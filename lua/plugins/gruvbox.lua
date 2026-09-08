221e026e6a30e305df42fcfdfd14e464b16e73d2
Matthew <mabla0531@gmail.com>
2026-09-07 20:26:16 -0500

    fix gruvbox underline slop (thanks clod)

--- lua/plugins/gruvbox.lua
+++ lua/plugins/gruvbox.lua
@@ -3,11 +3,8 @@ return {
     "ellisonleao/gruvbox.nvim",
     lazy = false,
     priority = 1000,
-    config = true,
     opts = {
       terminal_colors = true,
-      undercurl = false,
-      underline = true,
       bold = false,
       italic = {
         strings = false,
@@ -18,5 +15,21 @@ return {
       },
       invert_selection = true,
     },
+    config = function()
+      local gruvbox = require("gruvbox");
+      local palette = gruvbox.palette;
+
+      gruvbox.setup({
+        overrides = {
+          GruvboxRedUnderline    = { underline = true, undercurl = false, sp = palette.bright_red },
+          GruvboxGreenUnderline  = { underline = true, undercurl = false, sp = palette.bright_green },
+          GruvboxYellowUnderline = { underline = true, undercurl = false, sp = palette.bright_yellow },
+          GruvboxBlueUnderline   = { underline = true, undercurl = false, sp = palette.bright_blue },
+          GruvboxPurpleUnderline = { underline = true, undercurl = false, sp = palette.bright_purple },
+          GruvboxAquaUnderline   = { underline = true, undercurl = false, sp = palette.bright_aqua },
+          GruvboxOrangeUnderline = { underline = true, undercurl = false, sp = palette.bright_orange },
+        },
+      });
+    end
   },
 }
